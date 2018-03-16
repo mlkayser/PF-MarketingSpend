6922456bac455cd2b26c6d92cb11ad7d9ba2d2c7
Jonathan Linn <jonathanjlinn@gmail.com>
2018-03-15 22:29:58 -0400

    Initial Commit for Excel JS Refactor...FUN FUN FUN

--- routes/template.js
+++ routes/template.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
@@ -12,7 +14,8 @@ router.post('/', function(req, res) {
     } else {
         res.header('Access-Control-Allow-Origin', '*');
     }
-
+    
+    console.log('step 1');
     // var input = {ownershipGroupNumber: 123, clubs: [{clubId: 'PF Club Id 1'},{clubId: 'PF Club Id 2'},{clubId: 'PF Club Id 3'},{clubId: 'PF Club Id 4'},{clubId: 'PF Club Id 5'},{clubId: 'PF Club Id 6'}]};
     var input = req.body;
 
@@ -78,17 +81,28 @@ function createExcelTemplate(input) {
         baseHeader.header.push(club.clubId);
     });
 
+    console.log('Step 2');
     var XLSX = require('xlsx');
+    console.log('Step 3');
     var ws = XLSX.utils.json_to_sheet(baseWs, baseHeader);
+    console.log('Step 4');
     var colChar = 'B';
+    
     // Sum Rows
     _.forEach(input.clubs, function(club) {
         // // Comment on Club ID
         // ws[colChar + '1'].c = [ { a: 'PlanetFitness', t: 'Club Name here'} ];
         // Sum
-        ws[colChar + '23'] = { t:'n', f: "SUM(" + colChar + "2:" + colChar + "22)", F:"" + colChar + "23:" + colChar + "23" };
+        ws[colChar + '23'] = {
+            t:'n',
+            f: "SUM(" + colChar + "2:" + colChar + "22)",
+            F:"" + colChar + "23:" + colChar + "23"
+        };
+        
         colChar = nextChar(colChar);
     });
+    
+    
 
     // Formatting
     var fmt = '$0.00'; // or '"$"#,##0.00_);[Red]\\("$"#,##0.00\\)' or any Excel number format
@@ -101,9 +115,9 @@ function createExcelTemplate(input) {
             /* find the data cell (range.s.r + 1 skips the header row of the worksheet) */
             var ref = XLSX.utils.encode_cell({r:i, c:x});
             /* if the particular row did not contain data for the column, the cell will not be generated */
-            if(!ws[ref]) continue;
+            if(!ws[ref]) {continue;}
             /* `.t == "n"` for number cells */
-            if(ws[ref].t != 'n') continue;
+            if(ws[ref].t !== 'n') {continue;}
             /* assign the `.z` number format */
             ws[ref].z = fmt;
         }
@@ -118,8 +132,12 @@ function createExcelTemplate(input) {
 
     var fn = './uploads/' + input.ownershipGroupNumber + '-template-' + new Date().toISOString().substr(0,10) + '.xlsx';
     var wb = { SheetNames:['DMA'], Sheets:{}};
-    wb.Sheets['DMA'] = ws;
-    XLSX.writeFile(wb, fn);
+    wb.Sheets.DMA = ws;
+    
+    var XLSX_style = require('xlsx-style');
+    
+    
+    XLSX_style.writeFile(wb, fn);
     return fn;
 }
 
